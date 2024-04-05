d1ed05cc8affef282dd84090cf000a508131ebc4
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-04-05 18:30:26 +0330

    model Staff added

---
+++ models.py
@@ -0,0 +1,9 @@
+class Staff():
+    def __init__(self, _id, name, surname, username, password, access_level, wrong_times):
+        self._id=_id
+        self.name=name
+        self.surname=surname
+        self.username=username
+        self.password=password
+        self.access_level=access_level
+        self.wrong_times=wrong_times
