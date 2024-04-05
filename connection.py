9354f0933704d0efe6db7d3f4dad7b340c4b88aa
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-04-05 12:28:29 +0330

    Registeration finished, hashing password finished, database changed and access_level and wrong_times fields addet to table users

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import pymysql
 
+
 class Connection():
     def __init__(self, host='127.0.0.1', user='root', password='root'):
         self.host = host
@@ -12,8 +13,24 @@ class Connection():
         query = "CREATE TABLE IF NOT EXISTS `qaenpower`.`users` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `name` VARCHAR(63) NOT NULL, `surname` VARCHAR(63) NOT NULL, `username` VARCHAR(63) NOT NULL, `password` VARCHAR(127) NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE, UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);"
         self.cursor.execute(query)
 
-    def create_user(self):
-        pass
+    def create_user(self, name, surname, username, password):
+        query = "INSERT INTO `qaenpower`.`users` (`name`, `surname`, `username`, `password`) VALUES (%s, %s, %s, %s);"
+        values = name, surname, username, password
+        try:
+            self.cursor.execute(query, values)
+            self.connection.commit()
+            return ("ok", 0)
+        except pymysql.err.IntegrityError as error:
+            return (f"نام کاربری {username} قبلا ثبت شده است", error)
+        except pymysql.err.DataError as error:
+            temp = str(error)
+            if 'surname' in temp:
+                temp='نام خانوادگی'
+            elif 'username' in temp:
+                temp='نام کاربری'
+            elif 'name' in temp: # اول این رو به عنوان اولی گذاشتم. اما همه همین ارور رو میدادن. چون توی سرنیم و یوزرنیم هم کلمه نیم به کار رفته :دی. جالب بود گفتم بنویسم.
+                temp='نام'
+            return (f"طول فیلد {temp} بیش از اندازه تعیین شده است", error)
 
     def insert_counter(self):
         pass
---
+++ functions.py
@@ -0,0 +1,27 @@
+import hashlib
+
+
+def hash_password(password: str):
+    salt = "mohammad pourmohammadi fallah"
+    new_password = (password+salt).encode()
+    # hashed_password = hashlib.md5(new_password)               # 32 raghame hex
+    # hashed_password = hashlib.sha1(new_password)              # 40 raghame hex          
+    # hashed_password = hashlib.sha224(new_password)            # 56 raghame hex   
+    # hashed_password = hashlib.sha256(new_password)            # 64 raghame hex   
+    hashed_password = hashlib.sha384(new_password)              # 96 raghame hex
+    # hashed_password = hashlib.sha512(new_password)            # 128 raghame hex   
+    # hashed_password = hashlib.sha3_224(new_password)          # 56 raghame hex   
+    # hashed_password = hashlib.sha3_256(new_password)          # 64 raghame hex   
+    # hashed_password = hashlib.sha3_384(new_password)          # 96 raghame hex   
+    # hashed_password = hashlib.sha3_512(new_password)          # 128 raghame hex   
+    # hashed_password = hashlib.shake_128(new_password)         # vaghti in ro estefade mikonim dakhele tabe e hexdigest ye voroodi behesh midim masalan 50 ba'd ta 100 ragham minevesht. neveshtam 2500 va ta 5000 ragham nevesht. ama bayad voroodi ro behesh bedim.
+    # hashed_password = hashlib.shake_256(new_password)         # vaghti in ro estefade mikonim dakhele tabe e hexdigest ye voroodi behesh midim masalan 50 ba'd ta 100 ragham minevesht. neveshtam 2500 va ta 5000 ragham nevesht. ama bayad voroodi ro behesh bedim.
+    hashed_password = hashed_password.hexdigest()
+    return hashed_password
+
+if __name__=='__main__':
+    word='salam'
+    print(len(hash_password(word)), hash_password(word))
+    p = input("Enter password: ")
+    if hash_password(p)=='a893ce44036b095ec672bc890dea516ab89069f77fc9f02aadc0c9308761302ae7c3e25bbdcd7dafd7899d0186703eeb':
+        print("Welcome")
