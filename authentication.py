435bba1ca1d451c57347f78a78d293847d290968
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2025-02-02 20:04:49 +0330

    fix clicking on today button bug

--- authentication.py
+++ authentication.py
@@ -1935,15 +1935,14 @@ class DatePicker(MyWindows):
             return None
     
     def check_if_ohter_parts_are_filled(self):
-        global number_of_all_logged_counters, number_of_all_counters
-        # print(f"{number_of_all_counters=}\n{number_of_all_logged_counters=}")
-        # print('-'*50)
+        global number_of_all_logged_counters, number_of_all_counters, date_picker
         today = self.get_date()
         last_log_date = self.connection.get_last_log_date()
         if (
             number_of_all_logged_counters != 0
             and number_of_all_logged_counters != number_of_all_counters
             and today >= last_log_date
+            and today != date_picker.get_date()
         ):
             msb.showwarning("هشدار", "لطفا داده سایر بخش ها تکمیل و ذخیره شود.")
             freeze = True
